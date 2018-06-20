bb3ceb8f31d55c93a5534b4270d54fe6c61571d5
paulagd <paulagomezduran@gmail.com>
2018-06-20 13:08:49 +0100

    all the code added

---
+++ extract_faceNet_faces.py
@@ -0,0 +1,164 @@
+# NOTE:RUN this before using this script:
+# export PYTHONPATH=$PYTHONPATH/Users/paulagomezduran/Desktop/THINKSMARTER/Face_Detector/FaceNet
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from scipy import misc
+import scipy
+import sys
+import os
+import argparse
+import tensorflow as tf
+import numpy as np
+import facenet
+import detect_face
+import random
+import cv2
+import util
+
+from IPython import embed
+
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+from time import sleep
+from scipy.special import expit
+from PIL import Image
+
+def overlay_bounding_boxes(raw_img, refined_bboxes,lw=3):
+  """Overlay bounding boxes of face on images.
+    Args:
+      raw_img:
+        A target image.
+      refined_bboxes:
+        Bounding boxes of detected faces.
+      lw:
+        Line width of bounding boxes. If zero specified,
+        this is determined based on confidence of each detection.
+    Returns:
+      None.
+  """
+
+  # Overlay bounding boxes on an image with the color based on the confidence.
+  for r in refined_bboxes:
+    _score = expit(r[4])
+    cm_idx = int(np.ceil(_score * 255))
+    rect_color = [int(np.ceil(x * 255)) for x in util.cm_data[cm_idx]]  # parula
+    _lw = lw
+    if lw == 0:  # line width of each bounding box is adaptively determined.
+      bw, bh = r[2] - r[0] + 1, r[3] - r[0] + 1
+      _lw = 1 if min(bw, bh) <= 20 else max(2, min(3, min(bh / 20, bw / 20)))
+      _lw = int(np.ceil(_lw * _score))
+
+    _r = [int(x) for x in r[:4]]
+    cv2.rectangle(raw_img, (_r[0], _r[1]), (_r[2], _r[3]), rect_color, _lw)
+
+  raw_img = cv2.cvtColor(raw_img, cv2.COLOR_RGB2BGR)
+
+def full_img_with_boxes(full_img,boxes,filename,output_dir, counter):
+    raw_img = full_img.copy()
+    overlay_bounding_boxes(raw_img,boxes)
+
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    bottomLeftCornerOfText = (1100,700)
+    fontScale              = 1
+    fontColor              = (255,255,255)
+    lineType               = 4
+
+    cv2.putText(raw_img, "Counter:"+ str(counter),
+        bottomLeftCornerOfText,
+        font,
+        fontScale,
+        fontColor,
+        lineType)
+
+    # cv2.imwrite(os.path.join(output_dir, filename +'_detected.jpg' ),raw_img)
+    return raw_img
+
+def create_FaceNet_network_Params(args):
+    print('--- Creating FACENET networks and loading parameters ---')
+    # os.system('unset PYTHONPATH')
+    # os.system('export PYTHONPATH=$PYTHONPATH/Users/paulagomezduran/Desktop/THINKSMARTER/Face_Detector/FaceNet')
+    with tf.Graph().as_default():
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
+        sess_config = tf.ConfigProto()
+        sess_config.gpu_options.per_process_gpu_memory_fraction = 0.90
+        with tf.Session(config=sess_config) as sess:
+            sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+            with sess.as_default():
+                pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
+    return [pnet, rnet, onet]
+
+def faceNet_Detection(img, output_dir, args, pnet, rnet, onet):
+    minsize = 20 # minimum size of face
+    threshold = [ 0.6, 0.7, 0.7 ]  # three steps's threshold
+    factor = 0.709 # scale factor
+    nrof_successfully_aligned = 0
+    scaled_matrix = np.array([])
+    detected_faces = np.array([])
+
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    filename = 'test_frame'
+    output_filename = os.path.join(output_dir, filename+'.png')
+
+    if not os.path.exists(output_filename) and img is not None:
+        if img.ndim<2:
+            print('Unable to align "%s"' % image_path)
+            # continue
+        img = img[:,:,0:3]
+
+        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+
+        nrof_faces = bounding_boxes.shape[0]
+        if nrof_faces>0:
+            det = bounding_boxes[:,0:4]
+            det_arr = []
+            img_size = np.asarray(img.shape)[0:2]
+            if nrof_faces>1:
+                if args.detect_multiple_faces:
+                    for i in range(nrof_faces):
+                        det_arr.append(np.squeeze(det[i]))
+                else:
+                    bounding_box_size = (det[:,2]-det[:,0])*(det[:,3]-det[:,1])
+                    img_center = img_size / 2
+                    offsets = np.vstack([ (det[:,0]+det[:,2])/2-img_center[1], (det[:,1]+det[:,3])/2-img_center[0] ])
+                    offset_dist_squared = np.sum(np.power(offsets,2.0),0)
+                    index = np.argmax(bounding_box_size-offset_dist_squared*2.0) # some extra weight on the centering
+                    det_arr.append(det[index,:])
+            else:
+                det_arr.append(np.squeeze(det))
+
+            scaled_matrix = np.empty((len(det_arr),args.image_size,args.image_size,3))
+            for i, det in enumerate(det_arr):
+                det = np.squeeze(det)
+                bb = np.zeros(4, dtype=np.int32)
+                bb[0] = np.maximum(det[0]-args.margin/2, 0)
+                bb[1] = np.maximum(det[1]-args.margin/2, 0)
+                bb[2] = np.minimum(det[2]+args.margin/2, img_size[1])
+                bb[3] = np.minimum(det[3]+args.margin/2, img_size[0])
+                cropped = img[bb[1]:bb[3],bb[0]:bb[2],:]
+                scaled = misc.imresize(cropped, (args.image_size, args.image_size), interp='bilinear')
+                nrof_successfully_aligned += 1
+                filename_base, file_extension = os.path.splitext(output_filename)
+                if args.detect_multiple_faces:
+                    output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
+                else:
+                    output_filename_n = "{}{}".format(filename_base, file_extension)
+                scaled_matrix[i] = scaled
+                # cv2.imwrite(output_filename_n,scaled)
+                # text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+
+            detected_faces = full_img_with_boxes(img,bounding_boxes,filename,output_dir, nrof_successfully_aligned)
+            print ('Number of croped images on the frame : %d' % ( nrof_successfully_aligned))
+            nrof_successfully_aligned=0
+
+        else:
+            print('Unable to align "%s"' % image_path)
+
+    return [scaled_matrix, nrof_successfully_aligned, detected_faces]
---
+++ predict_multiple_faces_files.py
@@ -0,0 +1,158 @@
+import os
+import cv2
+import numpy as np
+import argparse
+from contextlib import contextmanager
+import h5py
+import scipy.misc as misc
+from utils import load_data
+from keras.utils import np_utils
+
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+
+from sklearn.metrics import accuracy_score,mean_squared_error
+
+# MODELS
+from wide_resnet import WideResNet
+# from face_network import create_face_network
+
+# DETECTORS
+# export PYTHONPATH=$PYTHONPATH/home/paula/THINKSMARTER_/Face_Detector/FaceNet
+import extract_faceNet_faces as faceNet
+
+from IPython import embed
+
+# pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.18-4.06.hdf5"
+modhash = '89f56a39a78454e96379348bddd78c0d'
+
+# # NOTE: pretrained weights + ethnicity
+# ETHNIC = {0: 'Asian', 1: 'Caucasian', 2: "African", 3: "Hispanic"}
+# directory_files = ['test_images/','output_cropped_Images/']
+# weight_file = "weights.18-4.06.hdf5"
+# weights_ethnic_file = "weights_ethnic.hdf5"
+# means_ethnic = "means_ethnic.npy"
+
+# NOTE: weights of training the model with an ethnicity dataset
+# ETHNIC = {0: 'White', 1: 'Black', 2: "Asian", 3: "Indian", 4: "Others"}
+directory_files = ['test_images/','output_cropped_Images/'] #TW  = trained weights
+
+weight_file = "weights.18-4.06.hdf5"
+# weight_file = "checkpoints/weights.09-4.32.hdf5"
+
+# weights_ethnic_file = "trained_weights/weights_ethnic_v1.hdf5"
+# means_ethnic = 'trained_weights/means_ethnic_v1.npy'
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="This script detects faces from web cam input, "
+                                                 "and estimates age and gender for the detected faces.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--weight_file", type=str, default=None,
+                        help="path to weight file (e.g. weights.18-4.06.hdf5)")
+    parser.add_argument("--depth", type=int, default=16,
+                        help="depth of network")
+    parser.add_argument("--width", type=int, default=8,
+                        help="width of network")
+
+    # IDEA: ADD facenet args
+    parser.add_argument('--image_size', type=int,
+        help='Image size (height, width) in pixels.', default=182)
+    parser.add_argument('--max_age', type=int,
+        help='Max age range of the dataset.', default=100)
+    parser.add_argument('--margin', type=int,
+        help='Margin for the crop around the bounding box (height, width) in pixels.', default=44)
+    parser.add_argument('--random_order',
+        help='Shuffles the order of images to enable alignment using multiple processes.', action='store_true')
+    parser.add_argument('--gpu_memory_fraction', type=float,
+        help='Upper bound on the amount of GPU memory that will be used by the process.', default=1.0)
+    parser.add_argument('--detect_multiple_faces', type=bool,
+                        help='Detect and align multiple faces per image.', default=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def transform_image_etnicity_to_predict(im):
+	means = np.load(means_ethnic)
+	im = im - means
+	return np.array([im])
+
+
+def main():
+    args = get_args()
+    depth = args.depth
+    k = args.width
+    max_age = args.max_age + 1
+
+    scaled_matrix = np.empty([])
+
+    for files in os.listdir(directory_files[0]):
+        if files.endswith('.jpg') or files.endswith('.png'):
+            print ('-------Predicting image:  '+files+'-------')
+            img = cv2.imread(os.getcwd()+'/'+directory_files[0]+files)
+            # for face detection
+            output_directory = directory_files[1]+files.split('.')[0]
+            if not os.path.exists(output_directory):
+                print ("** Creating output_directory in "+output_directory+' ... **')
+                os.makedirs(output_directory)
+            [pnet, rnet, onet] = faceNet.create_FaceNet_network_Params(args)
+            [scaled_matrix , n_faces_detected, detected_faces_image] = faceNet.faceNet_Detection(img,output_directory, args, pnet, rnet, onet)
+
+            # Load model and weights of AGE-GENDER
+            img_size_age_gender = 64
+            img_size_ethnicity = 224
+            model_age_gender = WideResNet(img_size_age_gender, depth=depth, k=k, units_age=max_age)()
+            model_age_gender.load_weights(weight_file)
+            #
+            # # Load model and weights of ETHNICITY
+            # model_ethnicity = create_face_network(nb_class=4, hidden_dim=512, shape=(224, 224, 3))
+            # model_ethnicity.load_weights(weights_ethnic_file)
+
+            # Resize the images for each model
+            faces = np.empty((len(scaled_matrix), img_size_age_gender, img_size_age_gender, 3))
+            # faces1 = np.empty((len(scaled_matrix), img_size_ethnicity, img_size_ethnicity, 3))
+
+            for i in range(len(scaled_matrix)):
+                faces[i, :, :, :] = cv2.resize(scaled_matrix[i], (img_size_age_gender, img_size_age_gender))
+                # faces1[i, :, :, :] = cv2.resize(scaled_matrix[i], (img_size_ethnicity, img_size_ethnicity))
+
+            # # predict with ethnicity model
+            # # TODO : Reduce the number of predictions in a second / minute
+            # result_ethn = np.empty((len(faces1),4))
+            # for i in range(len(faces1)):
+            #     result_ethn[i] = model_ethnicity.predict(transform_image_etnicity_to_predict(faces1[i]))
+
+            #NOTE predict ages and genders of the detected faces
+            result_age_gend = model_age_gender.predict(faces)
+            predicted_genders = result_age_gend[0]
+            ages = np.arange(0, max_age).reshape(max_age, 1)
+            predicted_ages = result_age_gend[1].dot(ages).flatten()
+
+            cols = 5
+            rows = int(len(scaled_matrix)/cols) + 1
+
+            for i in range(len(scaled_matrix)):
+                plt.subplot(rows, cols, i + 1)
+                im = faces[i].astype(np.uint8)
+                plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+                plt.title("{}, {}".format(int(predicted_ages[i]),
+                                          "F" if predicted_genders[i][0]>0.5 else "M"))
+                plt.axis('off')
+                plt.subplots_adjust(hspace=0.6)
+                cv2.imwrite(output_directory+'/'+"{}_{}_".format(int(predicted_ages[i]),
+                                    "F" if predicted_genders[i][0] > 0.5 else "M") +"_id"+str(i)+".jpg", scaled_matrix[i])
+
+            plt.savefig(directory_files[1]+"result_"+str(int(len(scaled_matrix)))+".png")
+
+
+                # misc.imsave(output_directory_files[1]+"{}_{}_{}".format(int(predicted_ages[i]),
+                #                     "F" if predicted_genders[i][0] > 0.5 else "M",
+                #                      ETHNIC[np.argmax(result_ethn[i])]) +"_"+str(i)+".jpg", scaled_matrix[i])
+
+
+if __name__ == '__main__':
+    main()
