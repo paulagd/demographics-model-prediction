24a86f177dc6c641567e9fa0246886df5ba7d5da
paulagd <paulagomezduran@gmail.com>
2018-07-09 17:24:10 +0100

    modified crowd detection

--- detect_crowd_from_files.py
+++ detect_crowd_from_files.py
@@ -19,6 +19,33 @@ def get_args():
     args = parser.parse_args()
     return args
 
+def write_info_to_img(full_img, counter):
+
+    raw_img = full_img.copy()
+
+
+    toWrite = "Estimated counter:"+ str(counter)
+    bottomLeftCornerOfText = (full_img.shape[1]-300,full_img.shape[0]- full_img.shape[0] + 25)
+
+
+    font                   = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale              = 0.8
+    lineType               = 2
+
+    if np.mean(full_img[full_img.shape[0]-50,full_img.shape[1]-200,:]) > 180:
+        fontColor = (0,0,0)
+    else:
+        fontColor = (255,255,255)
+
+    cv2.putText(raw_img, toWrite,
+        bottomLeftCornerOfText,
+        font,
+        fontScale,
+        fontColor,
+        lineType)
+
+    return raw_img
+
 
 def main():
 
@@ -30,16 +57,18 @@ def main():
             img = cv2.imread(os.getcwd()+'/'+directory_files[0]+files)
             # for face detection
             output_directory = directory_files[1]
-            # if not os.path.exists(output_directory):
-            #     print ("** Creating output_directory in "+output_directory+' ... **')
-            #     os.makedirs(output_directory)
+            if not os.path.exists(output_directory):
+                print ("** Creating output_directory in "+output_directory+' ... **')
+                os.makedirs(output_directory)
 
             cc = CrowdCounting()
             number_person = cc.run(img)
 
+            img_written = write_info_to_img(img, number_person[0])
+
             print ('The estimated number of people is ')
             print(number_person[0])
-            # cv2.imwrite(output_directory+files,detected_faces_image)
+            cv2.imwrite(output_directory+files,img_written)
 
 
 if __name__ == '__main__':
